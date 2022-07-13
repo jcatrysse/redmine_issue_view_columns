1547fb7f8360b0fe75dabf07b4d24b1b5cd94c6a
Alexandr Antonov <aas@altlinux.org>
2022-07-13 12:54:37 +0300

    for redmine 4.2

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
-require_dependency File.dirname(__FILE__) + "/lib/issue_view_columns/project_helper_patch.rb"
+require_dependency "issue_view_columns/project_helper_patch"
 
 Redmine::Plugin.register :redmine_issue_view_columns do
   name "Redmine Issue View Columns"
@@ -11,10 +11,6 @@ Redmine::Plugin.register :redmine_issue_view_columns do
     permission :manage_issue_view_columns, { issue_view_columns: :index }, { require: :member }
   end
   settings default: { "empty": true }, partial: "settings/issue_view_columns_settings"
-end
-
-if Rails.configuration.respond_to?(:autoloader) && Rails.configuration.autoloader == :zeitwerk
-  Rails.autoloaders.each { |loader| loader.ignore(File.dirname(__FILE__) + '/lib') }
 end
 
 # helper methods needed for the Settings page of the project also
