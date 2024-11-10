5f003316f4309977484c70065d665668e89683a3
siberianlove <74700646+siberianlove@users.noreply.github.com>
2024-11-10 15:49:11 +0300

    Create better_subtasks_table.js

---
+++ assets/javascripts/better_subtasks_table.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    obj = $("#issue_tree");
+    obj.attr("class", "autoscroll");
+    obj.attr("id", "custom_issue_tree");
+    obj = $("#relations");
+    obj.attr("class", "autoscroll");
+    obj.attr("id", "custom_relations");
+});
